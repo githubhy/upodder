118ad69bf291963b34056486d195c23a3a471c05
Asher Huang <huangyea@gmail.com>
2021-04-14 00:07:43 +0800

    Make cron use the environment of the container

--- upodder/config/start.sh
+++ upodder/config/start.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 cd /config
 [ -e config.json ] && trojan config.json >> /data/trojan.log 2>&1 &
+export > /root/.env
 cat crontab >> /data/uppoder.cron.log
+#echo "nameserver 8.8.8.8" > /etc/resolv.conf
+#echo "options ndots:0" >> /etc/resolv.conf
 crontab crontab
 crond -f
