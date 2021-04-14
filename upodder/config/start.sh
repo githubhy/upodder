af11f04e6d490080157d32c3ec31cba3e7e9c7e1
Asher Huang <huangyea@gmail.com>
2021-04-14 22:27:11 +0800

    Use flock to prevent duplicate cron jobs running

--- upodder/config/start.sh
+++ upodder/config/start.sh
@@ -1,9 +1,26 @@
 #!/usr/bin/env sh
-cd /config
-[ -e config.json ] && trojan config.json >> /data/trojan.log 2>&1 &
-export > /root/.env
-cat crontab >> /data/uppoder.cron.log
 #echo "nameserver 8.8.8.8" > /etc/resolv.conf
 #echo "options ndots:0" >> /etc/resolv.conf
-crontab crontab
+
+TROJAN_CONFIG_FILE="/config/config.json"
+UPODDER_CMD="/app/upodder/upodder.py --basedir=/config --podcastdir=/data --quiet"
+# Temporary files
+ENV_FILE="/tmp/.env"
+CRON_JOB_FILE="/tmp/cron_job.sh"
+CRONTAB_FILE="/tmp/crontab"
+# log files
+TROJAN_LOG_FILE="/data/trojan.log"
+UPODDER_LOG_FILE="/data/uppoder.cron.log"
+
+# Start trojan client
+[ -e ${TROJAN_CONFIG_FILE} ] && trojan ${TROJAN_CONFIG_FILE} >> ${TROJAN_LOG_FILE} 2>&1 &
+
+# Build the command and start a cron job scheduler
+export > ${ENV_FILE}
+echo "#!/usr/bin/env sh" > ${CRON_JOB_FILE}
+echo "source ${ENV_FILE}; ${UPODDER_CMD} >> ${UPODDER_LOG_FILE} 2>&1" >> ${CRON_JOB_FILE}
+chmod +x ${CRON_JOB_FILE}
+echo "*/30 * * * * /usr/bin/flock -n /tmp/fcj.lockfile ${CRON_JOB_FILE}" > ${CRONTAB_FILE}
+cat ${CRONTAB_FILE} >> ${UPODDER_LOG_FILE}
+crontab ${CRONTAB_FILE}
 crond -f
