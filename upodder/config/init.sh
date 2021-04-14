5b0d8fe2628773b250468d2b488c9d3b9ad1c85a
Asher Huang <huangyea@gmail.com>
2021-04-14 22:27:11 +0800

    Use flock to prevent duplicate cron jobs running

--- upodder/config/init.sh
+++ upodder/config/init.sh
@@ -2,6 +2,5 @@
 cd /config
 [ ! -e start.sh ] && cp /app/upodder/config/start.sh .
 [ ! -e subscriptions.yaml ] && cp /app/upodder/config/subscriptions.yaml .
-[ ! -e crontab ] && cp /app/upodder/config/crontab .
 
 sh start.sh
